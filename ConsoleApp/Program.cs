6e5b57faa5ff171ff53d81bdbe687c6a18e98266
omerfarukgulhan <omerfarukgulhan@outlook.com>
2024-07-14 19:21:13 +0300

    pdf created for phones in db

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,8 +3,13 @@ using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using QuestPDF.Previewer;
 using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleApp
 {
@@ -13,18 +18,59 @@ namespace ConsoleApp
         static void Main(string[] args)
         {
             IPhoneDal efPhoneDal = new EfPhoneDal();
-            Phone phone = efPhoneDal.Get(p => p.PhoneId == 1);
-            Console.WriteLine(phone.PhoneId);
-            Console.WriteLine(phone.Brand);
-            Console.WriteLine(phone.Model);
-
             IPhoneService phoneService = new PhoneManager(efPhoneDal);
             List<Phone> phoneList = phoneService.GetAll().Data;
 
-            foreach (var item in phoneList)
+            CreatePdfForPhones(phoneList);
+        }
+
+        private static void CreatePdfForPhones(List<Phone> phones)
+        {
+            QuestPDF.Settings.License = LicenseType.Community;
+            var document = Document.Create(container =>
             {
-                Console.WriteLine(item.PhoneId);
-            }
+                container.Page(page =>
+                {
+                    page.Size(PageSizes.A4);
+                    page.Margin(2, Unit.Centimetre);
+                    page.PageColor(Colors.White);
+                    page.DefaultTextStyle(x => x.FontSize(20));
+
+                    page.Header()
+                        .Text("Phone List")
+                        .SemiBold().FontSize(36).FontColor(Colors.Blue.Medium);
+
+                    page.Content()
+                        .PaddingVertical(1, Unit.Centimetre)
+                        .Column(column =>
+                        {
+                            column.Item().Text("List of Phones:");
+                            int index = 1;
+                            foreach (var phone in phones)
+                            {
+                                column.Item().Text($"{index}. Phone:");
+                                column.Item().Text($"   Brand: {phone.Brand}");
+                                column.Item().Text($"   Model: {phone.Model}");
+                                column.Item().Text($"   Release Date: {phone.ReleaseDate.ToShortDateString()}");
+                                column.Item().Text($"   Price: ${phone.Price}");
+                                column.Item().PaddingBottom(10);
+                                index++;
+                            }
+                        });
+
+                    page.Footer()
+                        .AlignCenter()
+                        .Text(x =>
+                        {
+                            x.Span("Page ");
+                            x.CurrentPageNumber();
+                        });
+                });
+            });
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "PhoneList.pdf");
+            document.GeneratePdf(filePath);
+            Console.WriteLine($"PDF document created at: {filePath}");
         }
     }
 }
